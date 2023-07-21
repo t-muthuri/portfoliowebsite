5abdc73476bcf896a36e0187f6a2fee316defc77
Tressie Muthuri <tressiemuthuri@gmail.com>
2023-07-21 09:27:31 +0300

    Build: responsive to mobile devices

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,9 @@
 import { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { IsObject, OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ( { isMobile } ) => {
 
   const computer = useGLTF
   ('./desktop_pc/scene.gltf')
@@ -23,8 +23,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={ isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]} 
         />
     </mesh>
@@ -32,6 +32,31 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add an event listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width:500px)");
+
+    // Set the initial value of the "isMobile state variable"
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener('change',
+    handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change',
+      handleMediaQueryChange);
+    }
+  }, [])
+
   return (
     <Canvas
       frameLoop="demand"
@@ -45,7 +70,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile}/>
       </Suspense>
       <Preload all />
     </Canvas>
